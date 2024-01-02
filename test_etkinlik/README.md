62d6f274c1610b0004b7d2d18690f7da31907975
Şeyma <symagn2@gmail.com>
2024-01-02 18:02:17 +0300

    calendar_test

---
+++ test_etkinlik/README.md
@@ -0,0 +1,24 @@
+## Test Senaryo Adı: Takvim Kontrolü Yapılacaktır.
+### Açıklama: Kullanıcının takvim sistemini test etmek için oluşturulmuştur.
+### Ön Koşul:  https://tobeto.com/ web sitesine erişmiş olmalıdır.
+
+### Test Case 1: Eğitim ve Etkinlik Sayfasının Erişim Kontrolü
+#### Adımlar:
+#### 1-) Sayfada yer alan eğitim ve etkinlik butonuna tıklayın.
+
+![Alt text](images/etkinliktakvimbuton.png)
+
+### Beklenen Sonuç:Ekranda "Eğitim ve Etkinlik Takvimi" sayfasına erişebilmelidir.
+
+![Alt text](images/etkinliksayfa.png)
+
+### Test Case 2: Eğitim ve Etkinlik Takvimi sayfasının kapatılması kontrolü
+#### Ön Koşul: Eğitim ve Etkinlik Takvimi sayfası açık olmalıdır.
+#### Adımlar:
+#### 1-) Kapat simgesine tıklayın.
+
+![Alt text](images/kapatmaikonu.png)
+
+#### Beklenen Sonuç:Kullanıcı https://tobeto.com/ sayfasına yönlendirilmelidir.
+
+## PYTEST KONTROLÜ 
---
+++ test_etkinlik/calendar.py
@@ -0,0 +1,42 @@
+
+import pytest
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from time import sleep
+
+
+class Test_calendar:
+  
+  def tobeto_giris(self):
+    self.driver = webdriver.Chrome()
+    self.driver.get("https://tobeto.com/")
+    self.driver.maximize_window() 
+  
+  #Eğitim ve Etkinlik Takvimine giriş
+  def test_calendar_login(self):
+    self.tobeto_giris()
+    calendar = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.CLASS_NAME, "calendar-btn")))
+    calendar.click()
+    sleep(3)
+  
+  #Eğitim ve Etkinlik Takviminden çıkış 
+  def test_calendar_out(self):
+    self.test_calendar_login()
+    sleep(3)
+    calendar_out = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.XPATH, "/html/body/div[4]/div/div/div[1]/button")))
+    calendar_out.click()
+    sleep(3)
+    
+    
+testClass = Test_calendar()
+testClass.test_calendar_login()
+testClass.test_calendar_out()
+
+  
