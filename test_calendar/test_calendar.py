8311819600eec8b20d6c92887fa3f49401a93b2d
Şeyma <symagn2@gmail.com>
2024-01-03 15:57:49 +0300

    test_calendar

--- test_calendar/test_calendar.py
+++ test_calendar/test_calendar.py
@@ -11,27 +11,27 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 
 
-class Test_calendar:
+class test_calendar:
   
   def tobeto_giris(self):
-    self.driver = webdriver.Chrome()
-    self.driver.get("https://tobeto.com/")
-    self.driver.maximize_window() 
+      self.driver = webdriver.Chrome()
+      self.driver.get("https://tobeto.com/")
+      self.driver.maximize_window() 
   
   #Eğitim ve Etkinlik Takvimine giriş
   def test_calendar_login(self):
-    self.tobeto_giris()
-    calendar = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.CLASS_NAME, "calendar-btn")))
-    calendar.click()
-    sleep(3)
+      self.tobeto_giris()
+      calendar = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.CLASS_NAME, "calendar-btn")))
+      calendar.click()
+      sleep(3)
   
   #Eğitim ve Etkinlik Takviminden çıkış 
   def test_calendar_out(self):
-    self.test_calendar_login()
-    sleep(3)
-    calendar_out = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.XPATH, "/html/body/div[4]/div/div/div[1]/button")))
-    calendar_out.click()
-    sleep(3)
+      self.test_calendar_login()
+      sleep(3)
+      calendar_out = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.XPATH, "/html/body/div[4]/div/div/div[1]/button")))
+      calendar_out.click()
+      sleep(3)
     
     
 
