83f0ad1e88076e4d6da8fb37e147c7c739bf1c44
Şeyma <symagn2@gmail.com>
2024-01-03 16:44:17 +0300

    test_calendar

--- test_calendar/test_calendar.py
+++ test_calendar/test_calendar.py
@@ -13,23 +13,24 @@ from time import sleep
 
 class test_calendar:
   
-  def tobeto_giris(self):
+
+  def test_login(self):
       self.driver = webdriver.Chrome()
       self.driver.get("https://tobeto.com/")
       self.driver.maximize_window() 
   
   #Eğitim ve Etkinlik Takvimine giriş
   def test_calendar_login(self):
-      self.tobeto_giris()
-      calendar = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.CLASS_NAME, "calendar-btn")))
+      self.test_login()
+      calendar = self.driver.find_element(By.CLASS_NAME, "calendar-btn")
       calendar.click()
       sleep(3)
-  
+    
   #Eğitim ve Etkinlik Takviminden çıkış 
   def test_calendar_out(self):
       self.test_calendar_login()
       sleep(3)
-      calendar_out = WebDriverWait(self.driver,6).until(ec.visibility_of_element_located((By.XPATH, "/html/body/div[4]/div/div/div[1]/button")))
+      calendar_out = self.driver.find_element(By.XPATH, "/html/body/div[4]/div/div/div[1]/button")
       calendar_out.click()
       sleep(3)
     
