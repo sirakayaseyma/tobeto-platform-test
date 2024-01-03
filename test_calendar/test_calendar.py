60e15ee4a70bce571632c63258fcc9dfe4bf2db9
Şeyma <symagn2@gmail.com>
2024-01-03 19:08:04 +0300

    test_calendar update

--- test_calendar/test_calendar.py
+++ test_calendar/test_calendar.py
@@ -11,28 +11,27 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 
 
-class test_calendar:
-  
+class Test_calendar:
 
-  def test_login(self):
-      self.driver = webdriver.Chrome()
-      self.driver.get("https://tobeto.com/")
-      self.driver.maximize_window() 
+    def test_login(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://tobeto.com/")
+        self.driver.maximize_window() 
   
-  #Eğitim ve Etkinlik Takvimine giriş
-  def test_calendar_login(self):
-      self.test_login()
-      calendar = self.driver.find_element(By.CLASS_NAME, "calendar-btn")
-      calendar.click()
-      sleep(3)
+    #Eğitim ve Etkinlik Takvimine giriş
+    def test_calendar_login(self):
+        self.test_login()
+        calendar = self.driver.find_element(By.CLASS_NAME, "calendar-btn")
+        calendar.click()
+        sleep(3)
     
-  #Eğitim ve Etkinlik Takviminden çıkış 
-  def test_calendar_out(self):
-      self.test_calendar_login()
-      sleep(3)
-      calendar_out = self.driver.find_element(By.XPATH, "/html/body/div[4]/div/div/div[1]/button")
-      calendar_out.click()
-      sleep(3)
+    #Eğitim ve Etkinlik Takviminden çıkış 
+    def test_calendar_out(self):
+        self.test_calendar_login()
+        sleep(3)
+        calendar_out = self.driver.find_element(By.XPATH, "/html/body/div[4]/div/div/div[1]/button")
+        calendar_out.click()
+        sleep(3)
     
     
 
