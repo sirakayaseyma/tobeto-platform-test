e68d46f36a53abcb2e5c9c1665d5410353f40d38
Şeyma <symagn2@gmail.com>
2024-01-03 17:49:32 +0300

    test_platform

---
+++ test_platform/constants/globalConstants.py
@@ -0,0 +1,8 @@
+PLATFORM_GIRIS_URL = "https://tobeto.com/giris"
+NAME_TAG = "email"
+PSW_TAG = "password"
+GIRIS_YAP_XPATH = "//*[@id='__next']/div/main/section/div/div/div[1]/div/form/button"
+LESSONS = "lessons-tab"
+NOTIFICATION = "notification-tab"
+SURVEY = "mySurvey-tab"
+APPLY = "apply-tab"
---
+++ test_platform/platform_1.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager . chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec 
+from time import sleep
+from constants import globalConstants as gc
+import openpyxl
+import pytest
+from selenium.webdriver.common.alert import Alert
+
+class Test_platform:   
+        def  giris_ekrani(self):
+            self.driver = webdriver.Chrome()
+            self.driver.get("https://tobeto.com/giris")
+            self.driver.maximize_window() 
+        
+        def tearDown(self):
+            self.driver.quit()
+ 
+        def test_basarili_giris(self):
+            self.giris_ekrani()
+            email = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.NAME, gc.NAME_TAG)))
+            email.send_keys("sirakaya.seymaa@gmail.com")
+            sleep(2)
+            psw = WebDriverWait(self.driver,3).until(ec.visibility_of_element_located((By.NAME, gc.PSW_TAG)))
+            psw.send_keys("123456")
+            loginbutton = self.driver.find_element(By.XPATH, gc.GIRIS_YAP_XPATH)
+            loginbutton.click()
+            sleep(2)
+            self.driver.execute_script("window.scrollBy(0, 500);")    # Aşağıya 200 piksel kaydırma
+            sleep(2)
+        
+        def test_platform(self):
+            self.test_basarili_giris()
+            #Eğitimlerim kontrolü
+            egitimlerim = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, gc.LESSONS)))
+            egitimlerim.click()
+            sleep(3)
+            
+            #Duyuru ve haber kontrolü
+            duyuruvehaber = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, gc.NOTIFICATION)))
+            duyuruvehaber.click()
+            sleep(3)
+
+            #Anketlerim kontrolü
+            anket = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, gc.SURVEY)))
+            anket.click()
+            sleep(3)
+            
+            #Basvuru kontrolü
+            basvuru = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID, gc.APPLY)))
+            basvuru.click()
+            sleep(3)
+            
+testClass = Test_platform()
+testClass.test_basarili_giris()
+testClass.test_platform()
