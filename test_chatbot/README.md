d82bc3d3c67038db8a32986f53d693cebff25711
Şeyma <symagn2@gmail.com>
2024-01-07 21:43:40 +0300

    test_chatbot

---
+++ test_chatbot/README.md
@@ -0,0 +1,25 @@
+## Test Senaryo Adı: Chatbot kontrolü yapılacaktır.
+#### Açıklama: Kullanıcının chatbot sistemini test etmek için oluşturulmuştur.
+#### Ön Koşul: Kullanıcı tobeto.com web sitesine erişmiş olmalıdır.
+## Test Case 1: Yeni mesaj bölümü kontrolü
+#### Adımlar:
+#### 1. Chatbot butonuna tıklayın.
+#### Beklenen sonuç:Ekranda açılır pencere şeklinde yeni bir mesaj bölümü açılmalıdır.
+
+### Test Case 2: Mesaj bölümü kapama kontrolü
+#### Ön Koşul: Mesaj bölümü açık olmalıdır.
+#### Adımlar:
+#### 1. Simge durumuna getir ikonuna tıklayın.
+#### Beklenen Sonuç: Mesaj bölümü kapanmalıdır.
+
+## Test Case 3: Uyarı Mesajı Kontrolü
+#### Ön Koşul: Mesaj bölümü açık olmalıdır.
+#### Adımlar:
+#### 1. Mesajlaşmayı bitirme ikonuna tıklayın.
+#### Beklenen Sonuç: Ekranda “Görüşmeyi bitirmek istediğinize emin misiniz?” Uyarı mesajı gözlenmelidir.
+
+## Test Case 4: Puanlama Tablosu Kontrolü
+#### Ön Koşul: Mesajlaşmayı bitirme ikonuna tıklanmış olmalıdır.
+#### Adımlar:
+#### 1) Görüşmeyi bitirmek istediğinize emin misiniz? Uyarı mesajında “Evet” butonuna tıklayın.
+#### Beklenen Sonuç:Ekranda görüşme hakkında geri bildirim almak için mesaj ekranına puanlama tablosu gelmelidir.
--- test_chatbot/chatbot.py
+++ test_chatbot/chatbot.py
@@ -6,12 +6,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 class test_Chatbot:
-    def test_chatbot_icon(self):
+    def giris(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://tobeto.com/")
         self.driver.maximize_window() 
-      
-        wait = WebDriverWait(self.driver, 10)   # Maksimum bekleme süresi (saniye cinsinden)
+    
+    # İkonun açılabilmesini sorgulamak için kullanılır.
+    def test_chatbot_icon(self):
+        self.giris()
+        wait = WebDriverWait(self.driver, 15)   # Maksimum bekleme süresi (saniye cinsinden)
         
         # İframe geçişini beklemek için implicit veya explicit bekleme kullanabilirsiniz.
         iframe = wait.until(EC.presence_of_element_located((By.ID, "exw-launcher-frame")))
@@ -21,6 +24,23 @@ class test_Chatbot:
         launcher_button = wait.until(EC.element_to_be_clickable((By.ID, "launcher")))
         launcher_button.click()
         sleep(10)
+                   
+    def test_chatbot_kapama(self):
+            self.test_chatbot_icon()
+            sleep(5)
+        
+        # CSS selektörü kullanarak SVG öğesini bulma
+            svg_element = self.driver.find_element(By.XPATH, "//*[@id='exw-conversation-frame-body']/div/div/div/div[1]/div/div[2]/svg[1]")
+
+        # SVG üzerinde tıklama
+            if svg_element:
+                svg_element.click()
+
+            # WebDriver'ı kapatma
+            self.driver.quit()
                     
+         
+             
 test_class = test_Chatbot()
-test_class.test_chatbot_icon()       
+test_class.test_chatbot_icon()
+#test_class.test_chatbot_kapama()
